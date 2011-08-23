10bf225f65082a774e4724fa1b99953d9a282cf5
Adam Taylor <adamjctaylor@gmail.com>
2011-08-23 09:09:54 +0100

    Add TweetMeme backlink plugin

--- lib/MightySEO/Backlinks/TweetMeme.pm
+++ lib/MightySEO/Backlinks/TweetMeme.pm
@@ -2,11 +2,22 @@ package MightySEO::Backlinks::TweetMeme;
 
 use Moose;
 use Carp;
+use WWW::TweetMeme;
 use DDP;
 
 sub get_backlinks {
     my $self = shift;
     my $url  = shift || croak 'URL required';
+
+    # TODO more robust URL construction
+    # TODO make sure each plugin uses the expected URL format
+    $url = 'http://'.$url;
+
+    my $resp = WWW::TweetMeme->get_url_info({
+        url => $url,
+    });
+
+    warn p $resp->{story}->{url_count};
 }
 
 1;
